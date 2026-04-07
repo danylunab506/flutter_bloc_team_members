5c558c003523009e7965173b92e86ab834f38483
Dany Luna <danyluna.b@gmail.com>
2026-04-06 19:17:03 -0600

    Create team members page base

---
+++ lib/features/team_members/presentation/pages/team_members_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TeamMembersPage extends StatelessWidget {
+  const TeamMembersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
